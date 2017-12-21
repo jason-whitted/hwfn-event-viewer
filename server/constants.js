9a431a22029e0692df3c8888c3e5eadd844bff5d
Jason Whitted <JWhitted@ent.com>
2017-12-21 11:05:40 -0700

    Create server constants and update meta api

---
+++ server/constants.js
@@ -0,0 +1,20 @@
+const APPLICATION = {
+  'Portal (Production)': 1,
+  'ORCC (Production)': 2,
+  'Lending Web Service (Production)': 3,
+  'Penley Discovery (Qualifile) Web Service (Production)': 4,
+  'AD Sync (Production)': 5,
+};
+
+const EVENT_TYPE = {
+  'Debug': 1,
+  'Usage': 2,
+  'Warning': 3,
+  'Error': 4,
+  'Notification': 5,
+};
+
+module.exports = {
+  APPLICATION,
+  EVENT_TYPE,
+};
