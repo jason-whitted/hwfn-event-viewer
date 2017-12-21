27150af1cb7eb38a7d47ee922d778d8d6c5f34fd
Jason Whitted <JWhitted@ent.com>
2017-12-21 10:35:05 -0700

    Update search api to return fake events

--- server/search/search.js
+++ server/search/search.js
@@ -1,12 +1,25 @@
+const faker = require('faker');
+
+const event = () => ({
+  id: faker.random.number(),
+  date: faker.date.past().toISOString(),
+  type: faker.random.number(),
+  appId: faker.random.number(),
+  desc: faker.lorem.text(),
+  info: faker.hacker.phrase(),
+  user: faker.internet.userName(),
+  ip: faker.internet.ip(),
+  version: '1.0.0.0',
+  eventId: faker.random.number(),
+});
+
 module.exports = router =>
   router.post('/api/search', (req, res) => {
     console.log(req.method, req.path);
     const { test } = req.body;
-    const cities = [
-      { name: 'New York City', population: 8175133 },
-      { name: 'Los Angeles', population: 3792621 },
-      { name: 'Chicago', population: 2695598 },
-      { name: test, population: Math.random() },
-    ];
-    res.json(cities);
+
+    const events = new Array(42).fill(0)
+      .map(event);
+
+    res.json({ events });
   });
