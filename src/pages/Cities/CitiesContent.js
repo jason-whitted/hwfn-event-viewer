d73b72783eb6d0bab5e43a7c9f4ce3c8de6f7f5b
Jason Whitted <jwhitted@ent.com>
2017-12-20 19:49:56 -0700

    Add express / concurrently

---
+++ src/pages/Cities/CitiesContent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class CitiesContent extends Component {
+  state = { cities: undefined };
+
+  componentWillMount() {
+    fetch('/api/cities')
+      .then(r => r.json())
+      .then(cities => {
+        this.setState({ cities });
+      });
+  }
+
+  render() {
+    const { cities } = this.state;
+    return (
+      <div>
+        {!cities && 'Loading...'}
+        {cities && <pre>{JSON.stringify(cities, null, 2)}</pre>}
+      </div>
+    );
+  }
+}
+
+export default CitiesContent;
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import { Cities } from './Cities';
+
 export default (
   <Switch>
     <Route exact path="/" render={() => 'Coming soon...'} />
+    <Route exact path="/cities" component={Cities} />
     <Route render={() => '404 - Not Found'} />
   </Switch>
 );
