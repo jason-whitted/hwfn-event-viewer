0556be3c8ef28279de841d1003517aa439513f88
Jason Whitted <JWhitted@ent.com>
2017-12-21 16:02:05 -0700

    Call search api and pass results to EventList

--- src/components/EventList/EventList.js
+++ src/components/EventList/EventList.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Module } from 'common';
 
-const EventList = () => (
+const EventList = ({ events }) => (
   <Module>
     <Module.Head>
       EventList
     </Module.Head>
     <Module.Body>
-      Coming soon...
+      <pre>
+        {JSON.stringify(events, null, 2)}
+      </pre>
     </Module.Body>
   </Module>
 );
+
+EventList.propTypes = {
+  events: PropTypes.array,
+};
+
+EventList.defaultProps = {
+  events: undefined,
+};
 
 export default EventList;
--- src/pages/Search/SearchContent.js
+++ src/pages/Search/SearchContent.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 
+import { apiProvider } from 'common';
 import { Search } from 'components/Search';
 import { EventList } from 'components/EventList';
 import { EventView } from 'components/EventView';
 
 class SearchContent extends Component {
+  state = { events: undefined };
+
   search = criteria => {
     console.log('SearchContent', 'search', { criteria });
+
+    const request = {
+      url: '/api/search',
+      method: 'POST',
+      data: criteria,
+    };
+
+    const success = ({ events }) => {
+      this.setState({ events });
+    };
+
+    const failure = error => {
+      console.error('SearchContent', 'search', error);
+    };
+
+    return apiProvider(request).then(success).catch(failure);
   };
 
   render = () => {
+    const { events } = this.state;
+
     return (
       <div>
         <div className="row">
@@ -17,7 +38,7 @@ class SearchContent extends Component {
             <Search search={this.search} />
           </div>
           <div className="col-xs-12 col-md-8">
-            <EventList />
+            <EventList events={events} />
           </div>
         </div>
         <div className="col-xs-12">
