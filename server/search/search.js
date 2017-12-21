95dc243371a4f9021966af46a1bbc831cc53385d
Jason Whitted <JWhitted@ent.com>
2017-12-21 09:44:12 -0700

    Update mock api to read from body

--- server/search/search.js
+++ server/search/search.js
@@ -1,10 +1,12 @@
 module.exports = router =>
   router.post('/api/cities', (req, res) => {
     console.log(req.method, req.path);
+    const { test } = req.body;
     const cities = [
       { name: 'New York City', population: 8175133 },
       { name: 'Los Angeles', population: 3792621 },
       { name: 'Chicago', population: 2695598 },
+      { name: test, population: Math.random() },
     ];
     res.json(cities);
   });
